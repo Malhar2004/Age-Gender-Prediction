546a5255e0cff8ec578a66bb9bb86ea49526479a
Malhar2004 <malharmr123@gmail.com>
2024-04-20 11:04:56 +0530

    Django integration added

---
+++ Age_Prediction/Age_prediction/predictions/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path, include
+from .views import AgeGenderView, interface
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('AgePred/', AgeGenderView.as_view(), name='detect_age_gender'),
+    path('', interface, name="interface")
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ Age_Prediction/Age_prediction/predictions/views.py
@@ -0,0 +1,120 @@
+from django.http import JsonResponse
+from django.views import View
+import cv2 as cv
+import math
+import time
+from django.conf import settings
+import os
+from django.shortcuts import render
+
+
+
+def getFaceBox(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+
+class AgeGenderView(View):
+    def get(self, request, *args, **kwargs):
+        input_source = request.GET.get('input', '0')  # Get input source from query parameter, default to camera
+        faceProto = os.path.join(settings.BASE_DIR,"predictions/models/opencv_face_detector.pbtxt")
+        faceModel = os.path.join(settings.BASE_DIR,"predictions/models/opencv_face_detector_uint8.pb")
+
+        ageProto = os.path.join(settings.BASE_DIR,"predictions/models/age_deploy.prototxt")
+        ageModel = os.path.join(settings.BASE_DIR,"predictions/models/age_net.caffemodel")
+
+        genderProto = os.path.join(settings.BASE_DIR,"predictions/models/gender_deploy.prototxt")
+        genderModel = os.path.join(settings.BASE_DIR,"predictions/models/gender_net.caffemodel")
+
+        MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+        ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+        genderList = ['Male', 'Female']
+
+        # Load network
+        ageNet = cv.dnn.readNet(ageModel, ageProto)
+        genderNet = cv.dnn.readNet(genderModel, genderProto)
+        faceNet = cv.dnn.readNet(faceModel, faceProto)
+
+        # Open a video file or an image file or a camera stream
+        cap = cv.VideoCapture(int(input_source))  # Convert input source to integer
+        padding = 20
+        processed = False  # Variable to track if a frame has been processed
+        while cv.waitKey(1) < 0:
+            if processed:
+                break  # If a frame has been processed, break out of the loop
+            # Read frame
+            t = time.time()
+            hasFrame, frame = cap.read()
+            if not hasFrame:
+                cv.waitKey()
+                break
+
+            frameFace, bboxes = getFaceBox(faceNet, frame)
+            if not bboxes:
+                print("No face Detected, Checking next frame")
+                continue
+
+            all_info_detected = True  # Variable to track if all information is detected for a face
+            for bbox in bboxes:
+                face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
+                       max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
+
+                blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+                genderNet.setInput(blob)
+                genderPreds = genderNet.forward()
+                gender = genderList[genderPreds[0].argmax()]
+                print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+
+                ageNet.setInput(blob)
+                agePreds = ageNet.forward()
+                age = ageList[agePreds[0].argmax()]
+                print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+
+                # label = "{},{}".format(gender, age)
+                # cv.putText(frameFace, label, (bbox[0], bbox[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2,
+                #            cv.LINE_AA)
+                # cv.imshow("Age Gender Demo", frameFace)
+
+                # If either gender or age information is not detected, set all_info_detected to False
+                if gender == 'Unknown' or age == 'Unknown':
+                    all_info_detected = False
+
+            # print("time : {:.3f}".format(time.time() - t))
+
+            # If all_info_detected is True, destroy all windows
+            if all_info_detected:
+                cv.destroyAllWindows()
+                processed = True  # Set processed to True to break out of the loop after processing this frame
+
+        # Instead of printing the results, store them in a dictionary
+        results = {
+            'age': age,
+            'agePreds': float(agePreds[0].max()),
+            'gender': gender,
+            'genderPreds':  float(genderPreds[0].max()),
+            # 'time': time.time() - t
+        }
+
+        # Return the results as a JSON response
+        return JsonResponse(results)
+
+def interface(request):
+    return render(request, "Age_prediction/index.html")
+
+
